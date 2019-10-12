80f01958422edd2a21033f6e3db40f0d028d198c
ajlopez <webmaster@ajlopez.com>
2019-10-12 12:46:44 -0300

    Name in lexer

---
+++ test/lexers.js
@@ -0,0 +1,22 @@
+const lexers = require('../lib/lexers');
+const TokenType = lexers.TokenType;
+
+exports['create lexer as object'] = function (test) {
+    const lexer = lexers.lexer('foo');
+    
+    test.ok(lexer);
+    test.equal(typeof lexer, 'object');
+};
+
+exports['first token'] = function (test) {
+    const lexer = lexers.lexer('foo');
+  
+    const token = lexer.next();
+    
+    test.ok(token);
+    test.equal(token.value, 'foo');
+    test.equal(token.type, TokenType.Name);
+    
+    test.equal(lexer.next(), null);
+};
+
