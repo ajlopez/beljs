d9e86e6ccd71d4fa1f04bd5b0c39c307d4bf6547
ajlopez <webmaster@ajlopez.com>
2019-10-15 13:08:23 -0300

    Create list

--- test/lists.js
+++ test/lists.js
@@ -9,3 +9,13 @@ exports['create pair'] = function (test) {
     test.equal(result.tail(), 2);
 };
 
+exports['create list with three elements'] = function (test) {
+    const result = lists.list([1, 2, 3]);
+    
+    test.ok(result);
+    test.equal(result.head(), 1);
+    test.equal(result.tail().head(), 2);
+    test.equal(result.tail().tail().head(), 3);
+    test.equal(result.tail().tail().tail(), null);
+};
+
