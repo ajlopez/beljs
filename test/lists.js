17eb47c3dbb14431299bd89b1c0fb624bb557ca1
ajlopez <webmaster@ajlopez.com>
2019-10-16 11:00:43 -0300

    Create list with three elements with non nil tail

--- test/lists.js
+++ test/lists.js
@@ -16,12 +16,22 @@ exports['create list with three elements'] = function (test) {
     test.equal(result.head(), 1);
     test.equal(result.tail().head(), 2);
     test.equal(result.tail().tail().head(), 3);
-    test.equal(result.tail().tail().tail(), null);
+    test.strictEqual(result.tail().tail().tail(), null);
 };
 
 exports['create list with zero elements as nil'] = function (test) {
     const result = lists.list([]);
     
     test.strictEqual(result, null);
+};
+
+exports['create list with three elements and non nil tail'] = function (test) {
+    const result = lists.list([1, 2, 3], 4);
+    
+    test.ok(result);
+    test.equal(result.head(), 1);
+    test.equal(result.tail().head(), 2);
+    test.equal(result.tail().tail().head(), 3);
+    test.equal(result.tail().tail().tail(), 4);
 };
 
