0823dddafa96715ff8fb811fb5360c46c0e8a59a
ajlopez <webmaster@ajlopez.com>
2019-11-15 12:53:46 -0300

    Removing geast usage; parser returns simple values

--- test/parser.js
+++ test/parser.js
@@ -1,16 +1,14 @@
 
-const geast = require('geast');
-
 const parser = require('../lib/parser');
 
 function parse(test, type, text, expected) {
-    test.deepEqual(geast.toObject(parser.parse(type, text)), expected);
+    test.deepEqual(parser.parse(type, text), expected);
 }
 
 exports['parse constants'] = function (test) {
-    parse(test, 'integer', '42', { ntype: 'constant', value: 42 });
-    parse(test, 'string', '"foo"', { ntype: 'constant', value: 'foo' });
-    parse(test, 'literal', 't', { ntype: 'constant', value: true });
-    parse(test, 'literal', 'nil', { ntype: 'constant' });
+    parse(test, 'integer', '42', 42);
+    parse(test, 'string', '"foo"', "foo");
+    parse(test, 'literal', 't', true);
+    parse(test, 'literal', 'nil', null);
 };
 
