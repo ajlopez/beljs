17024a1f5f1395fc3be7edb9331969e22ffe7660
ajlopez <webmaster@ajlopez.com>
2019-11-16 10:44:55 -0300

    Contexts

---
+++ test/contexts.js
@@ -0,0 +1,26 @@
+
+const contexts = require('../lib/contexts');
+
+exports['get unknown variable'] = function (test) {
+    const context = contexts.context();
+    
+    test.equal(context.get('foo'), null);
+};
+
+exports['set and get variable'] = function (test) {
+    const context = contexts.context();
+    
+    context.set('answer', 42);
+    
+    test.equal(context.get('answer'), 42);
+};
+
+exports['set variable in parent and get variable in child context'] = function (test) {
+    const parent = contexts.context();
+    const context = contexts.context(parent);
+    
+    parent.set('answer', 42);
+    
+    test.equal(context.get('answer'), 42);
+};
+
