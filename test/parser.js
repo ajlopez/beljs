49b1b6ebf6e632f32823d1184be7658a5669a5ca
ajlopez <webmaster@ajlopez.com>
2019-11-17 09:47:31 -0300

    Parse symbol

--- test/parser.js
+++ test/parser.js
@@ -1,8 +1,14 @@
 
 const parser = require('../lib/parser');
+const symbols = require('../lib/symbols');
 
 function parse(test, type, text, expected) {
-    test.deepEqual(parser.parse(type, text), expected);
+    const result = parser.parse(type, text);
+    
+    if (symbols.isSymbol(result) && symbols.isSymbol(expected))
+        test.strictEqual(result.name(), expected.name());
+    else
+        test.deepEqual(result, expected);
 }
 
 exports['parse constants'] = function (test) {
@@ -10,5 +16,9 @@ exports['parse constants'] = function (test) {
     parse(test, 'string', '"foo"', "foo");
     parse(test, 'literal', 't', true);
     parse(test, 'literal', 'nil', null);
+};
+
+exports['parse symbol'] = function (test) {
+    parse(test, 'symbol', 'foo', symbols.symbol('foo'));
 };
 
