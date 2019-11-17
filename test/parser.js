e10570cb8d4b96d865551f0a7b2b12ff7f7151f7
ajlopez <webmaster@ajlopez.com>
2019-11-17 10:16:13 -0300

    Parse list and expressions

--- test/parser.js
+++ test/parser.js
@@ -1,12 +1,15 @@
 
 const parser = require('../lib/parser');
 const symbols = require('../lib/symbols');
+const lists = require('../lib/lists');
 
 function parse(test, type, text, expected) {
     const result = parser.parse(type, text);
     
     if (symbols.isSymbol(result) && symbols.isSymbol(expected))
         test.strictEqual(result.name(), expected.name());
+    else if (lists.isList(result) && lists.isList(expected))
+        test.strictEqual(lists.toString(result), lists.toString(expected));
     else
         test.deepEqual(result, expected);
 }
@@ -20,5 +23,18 @@ exports['parse constants'] = function (test) {
 
 exports['parse symbol'] = function (test) {
     parse(test, 'symbol', 'foo', symbols.symbol('foo'));
+};
+
+exports['parse simple expressions'] = function (test) {
+    parse(test, 'expression', '42', 42);
+    parse(test, 'expression', '"foo"', "foo");
+    parse(test, 'expression', 't', true);
+    // TODO resolve null return in parser
+    // parse(test, 'expression', 'nil', null);
+    parse(test, 'expression', 'foo', symbols.symbol('foo'));
+};
+
+exports['parse list'] = function (test) {
+    parse(test, 'list', '(foo bar 42)', lists.list([ symbols.symbol('foo'), symbols.symbol('bar'), 42 ]));  
 };
 
