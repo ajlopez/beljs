e7fa0ef44614211895d081e3a1e5c369f1542a08
ajlopez <webmaster@ajlopez.com>
2019-11-19 13:06:20 -0300

    Evaluate list with primitive function

--- test/evaluate.js
+++ test/evaluate.js
@@ -2,6 +2,7 @@
 const bel = require('..');
 const contexts = require('../lib/contexts');
 const symbols = require('../lib/symbols');
+const lists = require('../lib/lists');
 
 exports['evaluate constants'] = function (test) {
     test.equal(bel.evaluate(42), 42);
@@ -19,5 +20,16 @@ exports['evaluate symbols'] = function (test) {
     
     test.strictEqual(bel.evaluate(foo, context), null);
     test.strictEqual(bel.evaluate(answer, context), 42);
+}
+
+exports['evaluate list with primitive function'] = function (test) {
+    const add = symbols.symbol('add');
+    const context = contexts.context();
+    
+    context.set('add', function (args) { return args[0] + args[1] });
+    
+    const list = lists.list([ add, 1, 41 ]);
+    
+    test.strictEqual(bel.evaluate(list, context), 42);
 }
 
