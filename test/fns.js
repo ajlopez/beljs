080999333109d9789f567c177a0cd3a59452515e
ajlopez <webmaster@ajlopez.com>
2019-12-02 11:27:00 -0300

    Create and evaluate fn with one argument

--- test/fns.js
+++ test/fns.js
@@ -1,5 +1,7 @@
 
 const fns = require('../lib/fns');
+const symbols = require('../lib/symbols');
+const lists = require('../lib/lists');
 
 exports['create fn with no arguments'] = function (test) {
     const fn = fns.fn(null, 42);
@@ -7,5 +9,15 @@ exports['create fn with no arguments'] = function (test) {
     test.ok(fn);
     test.ok(fns.isFn(fn));
     test.equal(fn.evaluate(), 42);
+};
+
+exports['create fn with one arguments'] = function (test) {
+    const x = symbols.symbol('x');
+    const args = lists.list([ x ]);
+    const fn = fns.fn(args, x);
+    
+    test.ok(fn);
+    test.ok(fns.isFn(fn));
+    test.equal(fn.evaluate([ 42 ]), 42);
 };
 
