a18727cf268a33ae6e4e79122aae11d6d26e8583
ajlopez <webmaster@ajlopez.com>
2019-12-19 09:42:04 -0300

    Execute set from inner context, additional test

--- test/execute.js
+++ test/execute.js
@@ -1,6 +1,12 @@
 
 const bel = require('..');
+const top = require('../lib/top');
 
 exports['execute function definition and apply it'] = function (test) {
     test.equal(bel.execute('(def add (x y) (+ x y)) (add 1 41)'), 42);
+};
+
+exports['execute set from inner context'] = function (test) {
+    test.equal(bel.execute('(def setanswer (x) (set answer x)) (setanswer 42)', top), 42);
+    test.equal(top.get('answer'), 42);    
 };
