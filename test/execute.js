1bdbd99362eca06dfb37a91140a1c0fe15e22b99
ajlopez <webmaster@ajlopez.com>
2019-12-21 14:04:52 -0300

    Additional test, execution function defined in another function

--- test/execute.js
+++ test/execute.js
@@ -10,3 +10,8 @@ exports['execute set from inner context'] = function (test) {
     test.equal(bel.execute('(def setanswer (x) (set answer x)) (setanswer 42)', top), 42);
     test.equal(top.get('answer'), 42);    
 };
+
+exports['execute function defined in another function'] = function (test) {
+    test.equal(bel.execute('(def defadd (x) (def add (y) (+ x y))) (defadd 2) (add 40)', top), 42);
+};
+
